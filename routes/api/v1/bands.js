49416f858dc700cbd55cb8129e4a0b3815e4e99c
Dan K <KC135Q@users.noreply.github.com>
2020-01-07 11:04:25 -0500

    Query on bands get method using band_id as an optional parameter

--- routes/api/v1/bands.js
+++ routes/api/v1/bands.js
@@ -1,17 +1,30 @@
 var express = require("express");
 var router = express.Router();
 
+var db = require("../../../models");
+
 /* Current route: /api/v1/bands */
 router
   .route("/:band_id?")
   .get((req, res) => {
-    res.send(`Getting`);
+    const queryConditional =
+      typeof req.params.band_id !== "undefined"
+        ? { where: { id: req.params.band_id } }
+        : {};
+    db.Band.findAll(queryConditional).then(bandList => {
+      res.json(bandList);
+    });
   })
   .put((req, res) => {
     res.send(`Putting ${req.params.band_id}`);
   })
   .post((req, res) => {
-    res.send(`Posting`);
+    db.Band.create({
+      name: req.body.name,
+      town: req.body.town
+    }).then(newBand => {
+      res.send(`Posted ${newBand.id}`);
+    });
   })
   .delete((req, res) => {
     res.send(`Deleting ${req.params.band_id}`);
