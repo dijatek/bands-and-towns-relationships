2035ff9476f4ead8c22d93a9e36339dfe164a9a6
Dan K <KC135Q@users.noreply.github.com>
2020-01-07 10:37:48 -0500

    router.route get put post and delete

--- routes/api/v1/bands.js
+++ routes/api/v1/bands.js
@@ -2,8 +2,19 @@ var express = require("express");
 var router = express.Router();
 
 /* Current route: /api/v1/bands */
-router.get("/", (req, res) => {
-  res.send("All the bands");
-});
+router
+  .route("/:band_id?")
+  .get((req, res) => {
+    res.send(`Getting`);
+  })
+  .put((req, res) => {
+    res.send(`Putting ${req.params.band_id}`);
+  })
+  .post((req, res) => {
+    res.send(`Posting`);
+  })
+  .delete((req, res) => {
+    res.send(`Deleting ${req.params.band_id}`);
+  });
 
 module.exports = router;
